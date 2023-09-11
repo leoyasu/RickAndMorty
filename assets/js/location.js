9455269ff5fcca80bdd07cfc2ee234ac38979b8c
LeandroYasutake <leandroyasutake@gmail.com>
2023-09-11 11:43:01 -0300

    fix spa locations

--- assets/js/location.js
+++ assets/js/location.js
@@ -1,231 +1,297 @@
 function displayLocation() {
   return `
-        
+  <div class="cards_container">
 
-  <div class="container">
-  <div class="row">
-    <div class="col">
-      <div class="card" style="width: 18rem;">
-        <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <div class="sub_data">
-                     <h4>Earth (C1-137)</h2>
-                     <h5>Dimension C-137</h5>
-                     <p>Planet</p>
-                     <h5>Residents: 27</h5>
-                     <h5>Created: 10/11/2017</h5>
-            </div>
-        </div>
+  <div class="card-all">
+
+    <div class="card">
+      <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p>Texto prueba</p>
+        <h4> Aca va otro texto prueba</h4>
+        <h5>ULTIMA PRUEBA</h5>
       </div>
     </div>
-    <div class="col">
-      <div class="card" style="width: 18rem;">
-        <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <div class="sub_data">
-                     <h4>Earth (C1-137)</h2>
-                     <h5>Dimension C-137</h5>
-                     <p>Planet</p>
-                     <h5>Residents: 27</h5>
-                     <h5>Created: 10/11/2017</h5>
-            </div>
-        </div>
+
+    <div class="card">
+      <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p>Texto prueba</p>
+        <h4> Aca va otro texto prueba</h4>
+        <h5>ULTIMA PRUEBA</h5>
       </div>
     </div>
-    <div class="col">
-      <div class="card" style="width: 18rem;">
-        <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <div class="sub_data">
-                     <h4>Earth (C1-137)</h2>
-                     <h5>Dimension C-137</h5>
-                     <p>Planet</p>
-                     <h5>Residents: 27</h5>
-                     <h5>Created: 10/11/2017</h5>
-            </div>
-        </div>
+
+    <div class="card">
+      <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p>Texto prueba</p>
+        <h4> Aca va otro texto prueba</h4>
+        <h5>ULTIMA PRUEBA</h5>
       </div>
     </div>
-    <div class="col">
-      <div class="card" style="width: 18rem;">
-        <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <div class="sub_data">
-                     <h4>Earth (C1-137)</h2>
-                     <h5>Dimension C-137</h5>
-                     <p>Planet</p>
-                     <h5>Residents: 27</h5>
-                     <h5>Created: 10/11/2017</h5>
-            </div>
-        </div>
+
+    <div class="card">
+      <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p>Texto prueba</p>
+        <h4> Aca va otro texto prueba</h4>
+        <h5>ULTIMA PRUEBA</h5>
       </div>
     </div>
+
+    <div class="card">
+      <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p>Texto prueba</p>
+        <h4> Aca va otro texto prueba</h4>
+        <h5>ULTIMA PRUEBA</h5>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p>Texto prueba</p>
+        <h4> Aca va otro texto prueba</h4>
+        <h5>ULTIMA PRUEBA</h5>
+      </div>
+    </div>
+
+    <div class="card">
+      <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p>Texto prueba</p>
+        <h4> Aca va otro texto prueba</h4>
+        <h5>ULTIMA PRUEBA</h5>
+      </div>
+    </div>
+
   </div>
 </div>
-      
-    `
+    `;
 }
 
- //   <div class= "cards_container">
-    //   <div class="card-all">
+//   <div class= "cards_container">
+//   <div class="card-all">
 
-    //      <!--TARJETA 1-->
+//      <!--TARJETA 1-->
 
-    //     <div class="cards">
-          
-    //         <img src="../assets/img/tarjeta.jpg"  alt="Imagen de Rick" />
-    //             <div class="sub_data">
-    //                 <h4>Earth (C1-137)</h2>
-    //                 <h5>Dimension C-137</h5>
-    //                 <p>Planet</p>
-    //                 <h5>Residents: 27</h5>
-    //                 <h5>Created: 10/11/2017</h5>
-    //             </div>
-              
-    //     </div>
+//     <div class="cards">
 
-    //     <!--TARJETA 2-->
+//         <img src="../assets/img/tarjeta.jpg"  alt="Imagen de Rick" />
+//             <div class="sub_data">
+//                 <h4>Earth (C1-137)</h2>
+//                 <h5>Dimension C-137</h5>
+//                 <p>Planet</p>
+//                 <h5>Residents: 27</h5>
+//                 <h5>Created: 10/11/2017</h5>
+//             </div>
 
-    //     <div class="cards">
-    //       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
-    //           <div class="sub_data">
-    //               <h4>Earth (C1-137)</h2>
-    //               <h5>Dimension C-137</h5>
-    //               <p>Planet</p>
-    //               <h5>Residents: 27</h5>
-    //               <h5>Created: 10/11/2017</h5>
-    //           </div>
-    //     </div>
+//     </div>
 
-    //     <!--TARJETA 3-->
+//     <!--TARJETA 2-->
 
-    //     <div class="cards">
-    //       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
-    //           <div class="sub_data">
-    //             <h4>Earth (C1-137)</h2>
-    //               <h5>Dimension C-137</h5>
-    //               <p>Planet</p>
-    //               <h5>Residents: 27</h5>
-    //               <h5>Created: 10/11/2017</h5>
-    //           </div>
-    //     </div>
+//     <div class="cards">
+//       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
+//           <div class="sub_data">
+//               <h4>Earth (C1-137)</h2>
+//               <h5>Dimension C-137</h5>
+//               <p>Planet</p>
+//               <h5>Residents: 27</h5>
+//               <h5>Created: 10/11/2017</h5>
+//           </div>
+//     </div>
 
+//     <!--TARJETA 3-->
 
-    //     <!--TARJETA 4-->
+//     <div class="cards">
+//       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
+//           <div class="sub_data">
+//             <h4>Earth (C1-137)</h2>
+//               <h5>Dimension C-137</h5>
+//               <p>Planet</p>
+//               <h5>Residents: 27</h5>
+//               <h5>Created: 10/11/2017</h5>
+//           </div>
+//     </div>
 
-    //     <div class="cards">
-    //       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
-    //           <div class="sub_data">
-    //             <h4>Earth (C1-137)</h2>
-    //               <h5>Dimension C-137</h5>
-    //               <p>Planet</p>
-    //               <h5>Residents: 27</h5>
-    //               <h5>Created: 10/11/2017</h5>
-    //           </div>
-    //     </div>
+//     <!--TARJETA 4-->
 
-    //     <!--TARJETA 5-->
-    //     <div class="cards">
-    //       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
-    //           <div class="sub_data">
-    //             <h4>Earth (C1-137)</h2>
-    //               <h5>Dimension C-137</h5>
-    //               <p>Planet</p>
-    //               <h5>Residents: 27</h5>
-    //               <h5>Created: 10/11/2017</h5>
-    //           </div>
-    //     </div>  
+//     <div class="cards">
+//       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
+//           <div class="sub_data">
+//             <h4>Earth (C1-137)</h2>
+//               <h5>Dimension C-137</h5>
+//               <p>Planet</p>
+//               <h5>Residents: 27</h5>
+//               <h5>Created: 10/11/2017</h5>
+//           </div>
+//     </div>
 
-    //     <!--TARJETA 6-->
-    //     <div class="cards">
-    //       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
-    //           <div class="sub_data">
-    //             <h4>Earth (C1-137)</h2>
-    //               <h5>Dimension C-137</h5>
-    //               <p>Planet</p>
-    //               <h5>Residents: 27</h5>
-    //               <h5>Created: 10/11/2017</h5>
-    //           </div>
-    //     </div>  
+//     <!--TARJETA 5-->
+//     <div class="cards">
+//       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
+//           <div class="sub_data">
+//             <h4>Earth (C1-137)</h2>
+//               <h5>Dimension C-137</h5>
+//               <p>Planet</p>
+//               <h5>Residents: 27</h5>
+//               <h5>Created: 10/11/2017</h5>
+//           </div>
+//     </div>
 
-    //     <!--TARJETA 7-->
-    //     <div class="cards">
-    //         <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
-    //             <div class="sub_data">
-    //               <h4>Earth (C1-137)</h2>
-    //                 <h5>Dimension C-137</h5>
-    //                 <p>Planet</p>
-    //                 <h5>Residents: 27</h5>
-    //                 <h5>Created: 10/11/2017</h5>
-    //             </div>
-    //     </div> 
+//     <!--TARJETA 6-->
+//     <div class="cards">
+//       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
+//           <div class="sub_data">
+//             <h4>Earth (C1-137)</h2>
+//               <h5>Dimension C-137</h5>
+//               <p>Planet</p>
+//               <h5>Residents: 27</h5>
+//               <h5>Created: 10/11/2017</h5>
+//           </div>
+//     </div>
 
-    //     <!--TARJETA 8-->
-    //     <div class="cards">
-    //       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
-    //           <div class="sub_data">
-    //             <h4>Earth (C1-137)</h2>
-    //               <h5>Dimension C-137</h5>
-    //               <p>Planet</p>
-    //               <h5>Residents: 27</h5>
-    //               <h5>Created: 10/11/2017</h5>
-    //           </div>
-    //    </div> 
+//     <!--TARJETA 7-->
+//     <div class="cards">
+//         <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
+//             <div class="sub_data">
+//               <h4>Earth (C1-137)</h2>
+//                 <h5>Dimension C-137</h5>
+//                 <p>Planet</p>
+//                 <h5>Residents: 27</h5>
+//                 <h5>Created: 10/11/2017</h5>
+//             </div>
+//     </div>
 
-    //   <!--TARJETA 9-->
-      
-    //     <div class="cards">
-    //         <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
-    //             <div class="sub_data">
-    //               <h4>Earth (C1-137)</h2>
-    //                 <h5>Dimension C-137</h5>
-    //                 <p>Planet</p>
-    //                 <h5>Residents: 27</h5>
-    //                 <h5>Created: 10/11/2017</h5>
-    //             </div>
-    //     </div> 
+//     <!--TARJETA 8-->
+//     <div class="cards">
+//       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
+//           <div class="sub_data">
+//             <h4>Earth (C1-137)</h2>
+//               <h5>Dimension C-137</h5>
+//               <p>Planet</p>
+//               <h5>Residents: 27</h5>
+//               <h5>Created: 10/11/2017</h5>
+//           </div>
+//    </div>
 
-    //     <!--TARJETA 10-->
+//   <!--TARJETA 9-->
 
-    //     <div class="cards">
-    //       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
-    //           <div class="sub_data">
-    //             <h4>Earth (C1-137)</h2>
-    //               <h5>Dimension C-137</h5>
-    //               <p>Planet</p>
-    //               <h5>Residents: 27</h5>
-    //               <h5>Created: 10/11/2017</h5>
-    //           </div>
-    //     </div> 
+//     <div class="cards">
+//         <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
+//             <div class="sub_data">
+//               <h4>Earth (C1-137)</h2>
+//                 <h5>Dimension C-137</h5>
+//                 <p>Planet</p>
+//                 <h5>Residents: 27</h5>
+//                 <h5>Created: 10/11/2017</h5>
+//             </div>
+//     </div>
 
-    //   <!--TARJETA 11-->
+//     <!--TARJETA 10-->
 
-    //   <div class="cards">
-    //     <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
-    //         <div class="sub_data">
-    //           <h4>Earth (C1-137)</h2>
-    //             <h5>Dimension C-137</h5>
-    //             <p>Planet</p>
-    //             <h5>Residents: 27</h5>
-    //             <h5>Created: 10/11/2017</h5>
-    //         </div>
-    //   </div> 
+//     <div class="cards">
+//       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
+//           <div class="sub_data">
+//             <h4>Earth (C1-137)</h2>
+//               <h5>Dimension C-137</h5>
+//               <p>Planet</p>
+//               <h5>Residents: 27</h5>
+//               <h5>Created: 10/11/2017</h5>
+//           </div>
+//     </div>
 
-    // <!--TARJETA 12-->
+//   <!--TARJETA 11-->
 
-    //     <div class="cards">
-    //       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
-    //           <div class="sub_data">
-    //             <h4>Earth (C1-137)</h2>
-    //               <h5>Dimension C-137</h5>
-    //               <p>Planet</p>
-    //               <h5>Residents: 27</h5>
-    //               <h5>Created: 10/11/2017</h5>
-    //           </div>
-    //     </div>
-        
-    //     <!--FIN DE TARJETAS-->
+//   <div class="cards">
+//     <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
+//         <div class="sub_data">
+//           <h4>Earth (C1-137)</h2>
+//             <h5>Dimension C-137</h5>
+//             <p>Planet</p>
+//             <h5>Residents: 27</h5>
+//             <h5>Created: 10/11/2017</h5>
+//         </div>
+//   </div>
+
+// <!--TARJETA 12-->
+
+//     <div class="cards">
+//       <img src="../assets/img/tarjeta.jpg" alt="Imagen de Rick" />
+//           <div class="sub_data">
+//             <h4>Earth (C1-137)</h2>
+//               <h5>Dimension C-137</h5>
+//               <p>Planet</p>
+//               <h5>Residents: 27</h5>
+//               <h5>Created: 10/11/2017</h5>
+//           </div>
+//     </div>
+
+//     <!--FIN DE TARJETAS-->
+
+//   <div class="container">
+//   <div class="row">
+//     <div class="col">
+//       <div class="card" style="width: 18rem;">
+//         <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
+//         <div class="card-body">
+//           <h5 class="card-title">Card title</h5>
+//           <div class="sub_data">
+//                      <h4>Earth (C1-137)</h2>
+//                      <h5>Dimension C-137</h5>
+//                      <p>Planet</p>
+//                      <h5>Residents: 27</h5>
+//                      <h5>Created: 10/11/2017</h5>
+//             </div>
+//         </div>
+//       </div>
+//     </div>
+//     <div class="col">
+//       <div class="card" style="width: 18rem;">
+//         <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
+//         <div class="card-body">
+//           <h5 class="card-title">Card title</h5>
+//           <div class="sub_data">
+//                      <h4>Earth (C1-137)</h2>
+//                      <h5>Dimension C-137</h5>
+//                      <p>Planet</p>
+//                      <h5>Residents: 27</h5>
+//                      <h5>Created: 10/11/2017</h5>
+//             </div>
+//         </div>
+//       </div>
+//     </div>
+//     <div class="col">
+//       <div class="card" style="width: 18rem;">
+//         <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
+//         <div class="card-body">
+//           <h5 class="card-title">Card title</h5>
+//           <div class="sub_data">
+//                      <h4>Earth (C1-137)</h2>
+//                      <h5>Dimension C-137</h5>
+//                      <p>Planet</p>
+//                      <h5>Residents: 27</h5>
+//                      <h5>Created: 10/11/2017</h5>
+//             </div>
+//         </div>
+//       </div>
+//     </div>
+//     <div class="col">
+//       <div class="card" style="width: 18rem;">
+//         <img src="../assets/img/tarjeta.jpg" class="card-img-top" alt="...">
+//         <div class="card-body">
+//           <h5 class="card-title">Card title</h5>
+//           <div class="sub_data">
+//                      <h4>Earth (C1-137)</h2>
+//                      <h5>Dimension C-137</h5>
+//                      <p>Planet</p>
+//                      <h5>Residents: 27</h5>
+//                      <h5>Created: 10/11/2017</h5>
+//             </div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
