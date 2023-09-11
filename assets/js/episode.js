319e66437233fd0b2dec1730a900d0f8fd9816ff
LeandroYasutake <leandroyasutake@gmail.com>
2023-09-11 20:41:57 -0300

    agregando episode to spa, falta sacar hardcodeo

--- assets/js/episode.js
+++ assets/js/episode.js
@@ -1,104 +1,98 @@
-function displayCharacter (){
+function displayEpisode (){
     return `
 
     <section id="cards_main">
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-        <div class="subcards">
-          <h2>episode</h2>
-          <p>december 2,2013</p>
-          <h1>S10E01</h1>
-          <a href="./info.html"><button>+info</button></a>
-        </div>
-      </section>
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+          
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+          <div class="subcards">
+            <div class="backgroundImg">
+              <h2>episode</h2>
+            <p>december 2,2013</p>
+            <h1>S10E01</h1>
+            </div>
+            <div class="divbutton">
+              <a href="./info.html"><button>+info</button></a>
+            </div>
+          </div>
+
+          
+          </section>
 
       `
 }
